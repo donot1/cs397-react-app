9d57d161fb4d2713a750a28a3957a0d90626d3f9
donot1 <kalghatgin@gmail.com>
2022-10-04 10:33:37 -0500

    fetching courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,39 @@
 import { useState } from 'react';
 import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Banner from './components/Banner';
-import CourseList from './components/CourseList';
 import './App.css';
 
+import CourseList from './components/CourseList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useJsonQuery } from "./utilities/fetch";
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const queryClient = new QueryClient(); 
+
+const Schedule = () => {
+  const [schedule, isLoading, error] = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
+  if (error) {
+    return <p> Error loading schedule.</p>;
   }
-};
+  else if (isLoading) {
+    return <p>Loading schedule...</p>;
+  }
+  return (
+    <div>
+      <h1>{schedule.title}</h1>
+      <CourseList courses={schedule.courses}></CourseList>
+    </div>
+  );
+}
 
 const App = () => {
   return (
-      <div className="container">
-        <Banner title={schedule.title}/>
-        <CourseList courses={schedule.courses}/>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="container">
+          <Schedule />
+        </div>
+      </QueryClientProvider>
   );
 }
 
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import "./Course.css";
 
 const Course = ({course}) => {
   return(
-        <div className="card m-1 p-2">
-            <div className="card-body">
-                <h5 className="card-title text-center">{course.term} CS {course.number}</h5>
-                <div className="card-text text-center">{course.title}</div>
-                <div className="card-footer bg-white">{course.meets}</div>
-            </div>
+    <div className="card m-1 p-2">
+      <div className="card-body">
+        <h5 className="card-title">{course.term} CS {course.number}</h5>
+        <div className="card-text">{course.title}</div>
         </div>
+        <div className="card-footer bg-white">
+          <p className="card-text">{course.meets}</p>
+        </div>
+    </div>
   );
 }
 
