3d34fd627b2ff9c123478342ef92a87b0595665e
donot1 <kalghatgin@gmail.com>
2022-11-09 10:50:07 -0600

    test for content

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 
 const Course = ({id, course, selected, conflicts, toggleSelected}) => {
   return(
-    <div className="card m-1 p-2" onClick={() => toggleSelected(id)}>
+    <div className="card m-1 p-2" 
+         data-cy="course"
+         onClick={() => toggleSelected(id)}>
       <div className={`card-body ${selected.includes(id) ? 'selected' : ''} 
                                  ${conflicts.includes(id)? 'conflict' : ''}`}>
         <h5 className="card-title">{course.term} CS {course.number}</h5>
